33fc9123ab334be9bb5f9a55bdf8b4fb036d4077
Otávio Luiz <otaviolmsantos@gmail.com>
2024-12-23 15:29:47 -0300

    Evolução do layout da página inicial interna

--- src/main/kotlin/com/pscarpellini/frontend/pages/restritos/Inicio.kt
+++ src/main/kotlin/com/pscarpellini/frontend/pages/restritos/Inicio.kt
@@ -2,6 +2,7 @@ package com.pscarpellini.frontend.pages.restritos
 
 import com.pscarpellini.frontend.enums.ArredondamentosEnum
 import com.pscarpellini.frontend.enums.IconesEnum
+import com.pscarpellini.frontend.enums.ItensMenuEnum
 import com.pscarpellini.frontend.fragments.geral.icone.icone
 import com.pscarpellini.frontend.fragments.geral.card.card
 import com.pscarpellini.frontend.fragments.geral.html_header.includeHtmlHeader
@@ -11,9 +12,10 @@ import com.pscarpellini.frontend.fragments.logados.header_logado.includeHeaderLo
 import com.pscarpellini.frontend.fragments.logados.menu_principal.includeMenuPrincipal
 import com.pscarpellini.models.vos.SessaoUsuarioVO
 import kotlinx.html.HTML
+import kotlinx.html.a
 import kotlinx.html.body
 
-fun HTML.homePage(
+fun HTML.inicio(
     sessao: SessaoUsuarioVO
 ) {
     includeHtmlHeader(
@@ -30,21 +32,29 @@ fun HTML.homePage(
                 colunas = 4,
                 classes = "w-full"
             ) {
-                card(arredondamento = ArredondamentosEnum.PILL, classes = "gap-2") {
-                    icone(IconesEnum.ADICIONAR, showBackground = true)
-                    +"Criar nova promoção"
+                a(href = ItensMenuEnum.PROMOCOES.caminho) {
+                    card(arredondamento = ArredondamentosEnum.PILL, classes = "gap-2 cursor-pointer hover:scale-105 hover:shadow-sm transition-all") {
+                        icone(IconesEnum.ADICIONAR, showBackground = true)
+                        +"Criar nova promoção"
+                    }
                 }
-                card(arredondamento = ArredondamentosEnum.PILL, classes = "gap-2") {
-                    icone(IconesEnum.PRECO, showBackground = true)
-                    +"Modificar saldo"
+                a(href = ItensMenuEnum.SALDOS_DOS_PROMOTORES.caminho) {
+                    card(arredondamento = ArredondamentosEnum.PILL, classes = "gap-2 cursor-pointer hover:scale-105 hover:shadow-sm transition-all") {
+                        icone(IconesEnum.PRECO, showBackground = true)
+                        +"Modificar saldo"
+                    }
                 }
-                card(arredondamento = ArredondamentosEnum.PILL, classes = "gap-2") {
-                    icone(IconesEnum.USUARIOS, showBackground = true)
-                    +"Cadastrar novo usuário"
+                a(href = ItensMenuEnum.GERENCIAMENTO_DE_USUARIOS.caminho) {
+                    card(arredondamento = ArredondamentosEnum.PILL, classes = "gap-2 cursor-pointer hover:scale-105 hover:shadow-sm transition-all") {
+                        icone(IconesEnum.USUARIOS, showBackground = true)
+                        +"Cadastrar novo usuário"
+                    }
                 }
-                card(arredondamento = ArredondamentosEnum.PILL, classes = "gap-2") {
-                    icone(IconesEnum.APP, showBackground = true)
-                    +"Configurações do app"
+                a(href = ItensMenuEnum.CONFIGURACOES_DO_APP.caminho) {
+                    card(arredondamento = ArredondamentosEnum.PILL, classes = "gap-2 cursor-pointer hover:scale-105 hover:shadow-sm transition-all") {
+                        icone(IconesEnum.APP, showBackground = true)
+                        +"Configurações do app"
+                    }
                 }
             }
             includeContentGrid(
