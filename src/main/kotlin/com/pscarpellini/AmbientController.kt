8ef19c496d878490fa07398eb9fca75ab0918318
Pedro <pedro.scarpellini@gmail.com>
2025-01-02 02:43:01 -0300

    * Mudança do banco de dados para o render

--- src/main/kotlin/com/pscarpellini/AmbientController.kt
+++ src/main/kotlin/com/pscarpellini/AmbientController.kt
@@ -3,4 +3,5 @@ package com.pscarpellini
 object AmbientController {
     const val encaminhamentoWhatsapp = 5519993994583
     val isDevelopment = false
+    val useNeon = false
 }
--- src/main/kotlin/com/pscarpellini/plugins/Database.kt
+++ src/main/kotlin/com/pscarpellini/plugins/Database.kt
@@ -7,14 +7,20 @@ import org.jetbrains.exposed.sql.*
 fun Application.configureDatabases() {
     if (AmbientController.isDevelopment)
         Database.connect(
-            "jdbc:postgresql://localhost:5432/decor_bonus_db",
-            user = "postgres",
-            password = "Monib@009"
+            "jdbc:postgresql://dpg-ctomo7popnds73fj42qg-a.oregon-postgres.render.com:5432/lumenappsdb",
+            user = "lumenappsdb_user",
+            password = "eNAb9LCF95F2JTUGDUby7xrWEIUWin0q"
         )
-    else
+    else if (AmbientController.useNeon)
         Database.connect(
             "jdbc:postgresql://ep-blue-unit-a5d79dcb.us-east-2.aws.neon.tech:5432/comissaodb",
             user = "comissaodb_owner",
             password = "T3JMNw0IYkBK"
+        )
+    else
+        Database.connect(
+            "jdbc:postgresql://dpg-ctomo7popnds73fj42qg-a/lumenappsdb",
+            user = "lumenappsdb_user",
+            password = "eNAb9LCF95F2JTUGDUby7xrWEIUWin0q"
         )
 }
