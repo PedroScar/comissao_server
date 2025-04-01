5ea4290c8aaa29628538a2cbcc19edf7ea26f0cd
C1322741 Pedro Scarpellini Dos Santos <pscarpellini@gmail.com>
2025-03-31 23:15:27 -0300

    Adicao serializable

--- src/main/kotlin/com/pscarpellini/models/vos/ExtratoVO.kt
+++ src/main/kotlin/com/pscarpellini/models/vos/ExtratoVO.kt
@@ -4,6 +4,7 @@ import com.pscarpellini.database.utils.LocalDateTimeSerializer
 import kotlinx.serialization.Serializable
 import java.time.LocalDateTime
 
+@Serializable
 data class ExtratoVO(
     val contaResponsavel: ContaVO,
     val contaSaldo: ContaVO,
